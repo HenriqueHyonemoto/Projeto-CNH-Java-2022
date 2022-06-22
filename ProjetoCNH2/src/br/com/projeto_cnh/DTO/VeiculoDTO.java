6296a486d10425d6667549ea75982533fd558c47
MatheusPerozini <perozinimagalhesmatheus@gmail.com>
2022-06-22 09:09:21 -0300

    Novo VeiculoDTO

---
+++ ProjetoCNH2/src/br/com/projeto_cnh/DTO/VeiculoDTO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto_cnh.DTO;
+
+/**
+ *
+ * @author aluno
+ */
+public class VeiculoDTO {
+    private String placa, modelo, tipo;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
