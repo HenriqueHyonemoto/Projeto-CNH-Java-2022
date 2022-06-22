8502799c6b540802330a18965220927f87d879f4
MatheusPerozini <perozinimagalhesmatheus@gmail.com>
2022-06-22 09:22:53 -0300

    Novo instrutorDTO

---
+++ ProjetoCNH2/src/br/com/projeto_cnh/DAO/VeiculoDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto_cnh.DAO;
+
+import br.com.projeto_cnh.DTO.VeiculoDTO;
+
+import java.sql.*;
+/**
+ *
+ * @author aluno
+ */
+public class VeiculoDAO {
+    
+    public VeiculoDAO(){
+    }
+    
+    public ResultSet rs = null;
+    Statement stmt = null;
+    
+    public boolean
+}
---
+++ ProjetoCNH2/src/br/com/projeto_cnh/DTO/InstrutorDTO.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto_cnh.DTO;
+
+/**
+ *
+ * @author aluno
+ */
+public class InstrutorDTO {
+    private int id_instrutor;
+    private String nomeInstrutor, telefoneInstutor, cpfInstrutor, emailInstrutor;
+
+    public int getId_instrutor() {
+        return id_instrutor;
+    }
+
+    public void setId_instrutor(int id_instrutor) {
+        this.id_instrutor = id_instrutor;
+    }
+
+    public String getNomeInstrutor() {
+        return nomeInstrutor;
+    }
+
+    public void setNomeInstrutor(String nomeInstrutor) {
+        this.nomeInstrutor = nomeInstrutor;
+    }
+
+    public String getTelefoneInstutor() {
+        return telefoneInstutor;
+    }
+
+    public void setTelefoneInstutor(String telefoneInstutor) {
+        this.telefoneInstutor = telefoneInstutor;
+    }
+
+    public String getCpfInstrutor() {
+        return cpfInstrutor;
+    }
+
+    public void setCpfInstrutor(String cpfInstrutor) {
+        this.cpfInstrutor = cpfInstrutor;
+    }
+
+    public String getEmailInstrutor() {
+        return emailInstrutor;
+    }
+
+    public void setEmailInstrutor(String emailInstrutor) {
+        this.emailInstrutor = emailInstrutor;
+    }
+
+    
+}
