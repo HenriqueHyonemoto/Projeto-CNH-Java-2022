645e0d4dea037191007d7d8436bab268646d116f
MatheusPerozini <perozinimagalhesmatheus@gmail.com>
2022-06-22 10:08:41 -0300

    Corrigido nome da variavel

--- ProjetoCNH2/src/br/com/projeto_cnh/DAO/VeiculoDAO.java
+++ ProjetoCNH2/src/br/com/projeto_cnh/DAO/VeiculoDAO.java
@@ -6,7 +6,8 @@
 package br.com.projeto_cnh.DAO;
 
 import br.com.projeto_cnh.DTO.VeiculoDTO;
-
+import br.com.projeto_cnh.DTO.InstrutorDTO;
+import br.com.projeto_cnh.DAO.ConexaoDAO;
 import java.sql.*;
 /**
  *
@@ -15,10 +16,56 @@ import java.sql.*;
 public class VeiculoDAO {
     
     public VeiculoDAO(){
+        
     }
     
     public ResultSet rs = null;
     Statement stmt = null;
     
-    public boolean
+    public boolean inserirVeiculo(VeiculoDTO veiculoDTO, InstrutorDTO instrutorDTO){
+        try {
+            
+            
+            return true;
+        } catch(Exception err){
+            System.out.println("Erro VeiculoDAO.inserirVeiculo: " + err.getMessage());
+            return false;
+        } finally {
+            ConexaoDAO.ConectDB();
+        }
+    }
+    
+    public boolean excluirVeiculo(VeiculoDTO veiculoDTO, InstrutorDTO instrutorDTO){
+        try {
+            return true;
+        } catch(Exception err){
+            System.out.println("Erro VeiculoDAO.excluirVeiculo: " + err.getMessage());
+            return false;
+        } finally {
+            ConexaoDAO.ConectDB();
+        }
+    }
+    
+    public boolean alterarVeiculo(VeiculoDTO veiculoDTO){
+        try {
+            return true;
+        } catch(Exception err){
+            System.out.println("Erro VeiculoDAO.alterarVeiculo: " + err.getMessage());
+            return false;
+        } finally {
+            ConexaoDAO.ConectDB();
+        }
+    }
+    
+    public ResultSet consultarVeiculo(VeiculoDTO veiculoDTO, InstrutorDTO instrutorDTO){
+        try {
+            
+            
+            return rs;
+        } catch(Exception err){
+            System.out.println("Erro VeiculoDAO.alterarVeiculo: " + err.getMessage());
+            return rs;
+        }
+    }
+    
 }
